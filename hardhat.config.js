3018a8a08b22774b0e9b93bb4a156d1096ff10e1
FELIPE <wafflemakr@gmail.com>
2021-01-30 08:31:10 -0500

    hardhat env

---
+++ hardhat.config.js
@@ -0,0 +1,52 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require("dotenv").config();
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-web3");
+
+module.exports = {
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+        // blockNumber: 11589707,
+      },
+    },
+    live: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      accounts: [process.env.MAINNET_PRIVKEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API,
+    // url: "https://api-rinkeby.etherscan.io/",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.7.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  mocha: {
+    timeout: 240000,
+  },
+};
