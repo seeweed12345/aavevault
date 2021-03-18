841c3ec9407efaa014a99b05f9007a9761149ab2
FELIPE <wafflemakr@gmail.com>
2021-03-17 19:37:56 -0500

    balancer removing liquidity testing

--- test/balancer.test.js
+++ test/balancer.test.js
@@ -17,6 +17,7 @@ const FEE = 1000;
 
 // TOKENS
 const ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 const BPT = "0x7aFE74AE3C19f070c109A38C286684256ADC656C"; //WETH/DAI 50-50
 
@@ -25,11 +26,12 @@ const toWei = (value) => web3.utils.toWei(String(value));
 const fromWei = (value) => Number(web3.utils.fromWei(String(value)));
 
 contract("Balancer Logic", ([user, multisig]) => {
-  let registry, wallet, balancer, dai, bpt;
+  let registry, wallet, balancer, dai, bpt, weth;
 
   before(async function () {
     dai = await IERC20.at(DAI_ADDRESS);
     bpt = await IERC20.at(BPT);
+    weth = await IERC20.at(WETH_ADDRESS);
 
     const EthaRegistry = await ethers.getContractFactory("EthaRegistry");
 
@@ -129,5 +131,41 @@ contract("Balancer Logic", ([user, multisig]) => {
 
     const balance = await bpt.balanceOf(wallet.address);
     assert(balance > 0);
+  });
+
+  it("should remove ETH/DAI liquidity from balancer", async function () {
+    const initial = await weth.balanceOf(wallet.address);
+
+    const data = web3.eth.abi.encodeFunctionCall(
+      {
+        name: "removeLiquidity",
+        type: "function",
+        inputs: [
+          {
+            type: "address",
+            name: "poolAddress",
+          },
+          {
+            type: "address",
+            name: "tokenOut",
+          },
+          {
+            type: "uint256",
+            name: "poolAmtIn",
+          },
+        ],
+      },
+      [BPT, WETH_ADDRESS, MAX_UINT256]
+    );
+
+    const tx = await wallet.execute([balancer.address], [data], false, {
+      from: user,
+      gas: web3.utils.toHex(5e6),
+    });
+
+    console.log("\tGas Used:", tx.receipt.gasUsed);
+
+    const balance = await weth.balanceOf(wallet.address);
+    assert(balance > initial);
   });
 });
