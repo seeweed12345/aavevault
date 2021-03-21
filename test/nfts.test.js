c69a66e8e3fae96671c9622565b6a38fb8b2c484
FELIPE <wafflemakr@gmail.com>
2021-03-21 14:05:28 -0500

    tests for ERC1155 tokens

--- test/nfts.test.js
+++ test/nfts.test.js
@@ -14,8 +14,14 @@ const TOKEN_ID = 10260;
 const NFT_HOLDER = "0xc72aed14386158960d0e93fecb83642e68482e4b";
 const HASHMASKS = "0xc2c747e0f7004f9e8817db2ca4997657a7746928";
 
+// ERC1155
+const TOKEN_ID2 = 192593;
+const AMOUNT = 5;
+const NFT_HOLDER2 = "0x539c68764c0D676502A4EFa3355b53339542c4E2";
+const RARIBLE = "0xd07dc4262BCDbf85190C01c996b4C06a461d2430";
+
 contract("Smart Wallet", ([multisig, alice, bob, charlie]) => {
-  let registry, wallet, nftTransfers, erc721;
+  let registry, wallet, nftTransfers, erc721, erc1155;
 
   before(async function () {
     await hre.network.provider.request({
@@ -23,7 +29,13 @@ contract("Smart Wallet", ([multisig, alice, bob, charlie]) => {
       params: [NFT_HOLDER],
     });
 
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [NFT_HOLDER2],
+    });
+
     erc721 = await IERC721.at(HASHMASKS);
+    erc1155 = await IERC1155.at(RARIBLE);
 
     const EthaRegistry = await ethers.getContractFactory("EthaRegistry");
 
@@ -132,5 +144,125 @@ contract("Smart Wallet", ([multisig, alice, bob, charlie]) => {
     // New token owner.
     const owner = await erc721.ownerOf(TOKEN_ID);
     assert.equal(owner, charlie);
+  });
+
+  it("should be able to receive ERC1155 tokens", async function () {
+    await erc1155.safeTransferFrom(
+      NFT_HOLDER2,
+      wallet.address,
+      TOKEN_ID2,
+      AMOUNT,
+      "0x",
+      {
+        from: NFT_HOLDER2,
+      }
+    );
+
+    const balance = await erc1155.balanceOf(wallet.address, TOKEN_ID2);
+    assert.equal(balance, AMOUNT);
+  });
+
+  it("should be able to send out ERC1155 tokens", async function () {
+    const data = web3.eth.abi.encodeFunctionCall(
+      {
+        name: "transferERC1155",
+        type: "function",
+        inputs: [
+          {
+            type: "address",
+            name: "erc721",
+          },
+          {
+            type: "address",
+            name: "recipient",
+          },
+          {
+            type: "uint256",
+            name: "tokenId",
+          },
+          {
+            type: "uint256",
+            name: "quantity",
+          },
+        ],
+      },
+      [RARIBLE, bob, TOKEN_ID2, AMOUNT]
+    );
+
+    const tx = await wallet.execute([nftTransfers.address], [data], false, {
+      from: alice,
+      gas: web3.utils.toHex(5e6),
+    });
+    console.log("\tGas Used:", tx.receipt.gasUsed);
+
+    let balance = await erc1155.balanceOf(wallet.address, TOKEN_ID2);
+    assert.equal(balance, 0);
+
+    balance = await erc1155.balanceOf(bob, TOKEN_ID2);
+    assert.equal(balance, AMOUNT);
+  });
+
+  it("should be able to approve ERC1155 operator", async function () {
+    // send back tokens for testing approvals
+    await erc1155.safeTransferFrom(
+      bob,
+      wallet.address,
+      TOKEN_ID2,
+      AMOUNT,
+      "0x",
+      {
+        from: bob,
+      }
+    );
+
+    const data = web3.eth.abi.encodeFunctionCall(
+      {
+        name: "setApprovalERC1155",
+        type: "function",
+        inputs: [
+          {
+            type: "address",
+            name: "erc721",
+          },
+          {
+            type: "address",
+            name: "to",
+          },
+          {
+            type: "bool",
+            name: "status",
+          },
+        ],
+      },
+      [RARIBLE, bob, true]
+    );
+
+    const tx = await wallet.execute([nftTransfers.address], [data], false, {
+      from: alice,
+      gas: web3.utils.toHex(5e6),
+    });
+    console.log("\tGas Used:", tx.receipt.gasUsed);
+
+    const isApprovedForAll = await erc1155.isApprovedForAll(
+      wallet.address,
+      bob
+    );
+    assert(isApprovedForAll);
+
+    // Tokens transfered out of smart wallet
+    await erc1155.safeTransferFrom(
+      wallet.address,
+      charlie,
+      TOKEN_ID2,
+      AMOUNT,
+      "0x",
+      {
+        from: bob, // operator
+      }
+    );
+
+    // New tokens owner.
+    const balance = await erc1155.balanceOf(charlie, TOKEN_ID2);
+    assert.equal(balance, AMOUNT);
   });
 });
