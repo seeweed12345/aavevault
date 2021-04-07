cabb01f8c575048bc749f02970723cc51849e01e
blockchaindev520 <sparshjhamb@sparshs-MacBook-Air.local>
2021-04-07 13:17:09 +0530

    staking contracts script

---
+++ scripts/staking/deployFactory.js
@@ -0,0 +1,30 @@
+const StakingRewardsFactory = artifacts.require("StakingRewardsFactory");
+const etha = "0x59E9261255644c411AfDd00bD89162d09D862e38";
+const genesis = 1617710194;
+
+const toWei = (value) => web3.utils.toWei(String(value));
+
+const ETHA_ETH_LP = "0xd006f2de9423090359ab04d5858a981161ce9062";
+const ETHA_USDC_LP = "0xc6136ee4c911ed6b1cccaeee9065854435c0b5e0";
+
+const rewardAmount = toWei("90000");
+const rewardsDuration = 60*60*24*90; // 3 Months
+
+
+async function main() {
+
+  const factory = await StakingRewardsFactory.new(etha, genesis);
+  console.log("Factory Deployed:", factory.address);
+  await factory.deploy(ETHA_ETH_LP, rewardAmount, rewardsDuration);
+  console.log('deployed staking contract for ETHA_ETH');
+  await factory.deploy(ETHA_USDC_LP, rewardAmount, rewardsDuration);
+  console.log('deployed staking contract for ETHA_USDC');
+  console.log("\nDone!");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
