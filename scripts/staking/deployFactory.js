a3b9ab45a047f5fb52bede36f346f0c2692d9147
blockchaindev520 <sparshjhamb@sparshs-MacBook-Air.local>
2021-04-13 21:45:20 +0530

    updated staking contract deployment script

--- scripts/staking/deployFactory.js
+++ scripts/staking/deployFactory.js
@@ -4,8 +4,9 @@ const genesis = 1617710194;
 
 const toWei = (value) => web3.utils.toWei(String(value));
 
-const ETHA_ETH_LP = "0xd006f2de9423090359ab04d5858a981161ce9062";
-const ETHA_USDC_LP = "0xc6136ee4c911ed6b1cccaeee9065854435c0b5e0";
+const ETHA_ETH_UNI_LP = "0xd006f2de9423090359ab04d5858a981161ce9062";
+const ETHA_USDC_BALANCER_LP = "0x7B7499Fab431DeE48174f8A775E799DD1542f487";
+const ETHA_USDC_UNI_LP = "0xc6136ee4c911ed6b1cccaeee9065854435c0b5e0";
 
 const rewardAmount = toWei("90000");
 const rewardsDuration = 60*60*24*90; // 3 Months
@@ -15,10 +16,12 @@ async function main() {
 
   const factory = await StakingRewardsFactory.new(etha, genesis);
   console.log("Factory Deployed:", factory.address);
-  await factory.deploy(ETHA_ETH_LP, rewardAmount, rewardsDuration);
-  console.log('deployed staking contract for ETHA_ETH');
-  await factory.deploy(ETHA_USDC_LP, rewardAmount, rewardsDuration);
-  console.log('deployed staking contract for ETHA_USDC');
+  await factory.deploy(ETHA_ETH_UNI_LP, rewardAmount, rewardsDuration);
+  console.log('deployed staking contract for ETHA_ETH_UNI_LP');
+  await factory.deploy(ETHA_USDC_BALANCER_LP, rewardAmount, rewardsDuration);
+  console.log('deployed staking contract for ETHA_USDC_BALANCER_LP');
+  await factory.deploy(ETHA_USDC_UNI_LP, rewardAmount, rewardsDuration);
+  console.log('deployed staking contract for ETHA_USDC_UNI_LP');
   console.log("\nDone!");
 }
 
